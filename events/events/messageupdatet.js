4e92fcebe483ca2c85bf9b2392b0081a15c478f1
Voroniyx <91881990+Voroniyx@users.noreply.github.com>
2022-02-16 12:10:55 +0100

    idk, what have i changed

--- events/events/messageupdatet.js
+++ events/events/messageupdatet.js
@@ -11,13 +11,13 @@ const { MessageEmbed } = require('discord.js');
 client.on('messageUpdate', (oldMessage, newMessage) => { // Old message may be undefined
    if (!oldMessage.author) return;
    const MessageLog = client.channels.cache.find(channel => channel.id ==='912788049384050708');
-var embed = new MessageEmbed()
-.setAuthor(newMessage.author.username)
-.setTimestamp()
-.setColor('#392B47')
-.addField('Original:', `${oldMessage}`)
-.addField('Bearbeitet:', `${newMessage}`)
+   var embed = new MessageEmbed()
+   .setAuthor(newMessage.author.username)
+   .setTimestamp()
+   .setColor('#392B47')
+   .addField('Original:', `${oldMessage}`)
+   .addField('Bearbeitet:', `${newMessage}`)
 
 
-MessageLog.send({ embeds: [embed] });
+   MessageLog.send({ embeds: [embed] });
 })
